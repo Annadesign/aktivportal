d4fa38982a10b5160fb66cfe8c9daa7eb4e3a5c6
annadesign <konto@annadesign.no>
2018-12-05 19:16:00 +0100

    show events

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,6 +9,10 @@ class Event < ApplicationRecord
 		where("publish=true AND title ILIKE ? OR summary ILIKE ? OR content ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
 	end
 
+	def self.xmatching_title_or_content search
+		where("publish=true AND title ILIKE ? OR summary ILIKE ? OR content ILIKE ? AND location= #{:location}", "%#{search}%", "%#{search}%", "%#{search}%")
+	end	
+
 	def self.filter_by_tags param_tag
 		includes(:tags).where(publish: true, tags: {name: param_tag}).order(id: :asc)
 	end
